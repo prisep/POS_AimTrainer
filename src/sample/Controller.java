1f73217170dfd5e2ccebe85f67d7d0d801a57dd2
prisep <78471564+prisep@users.noreply.github.com>
2021-06-18 09:59:19 +0200

    Add files via upload

---
+++ src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+
+import java.awt.*;
+
+public class Controller {
+
+
+}
